55d0af877ff300b3da1d83d1f652015587abe617
Stas Kolesnik <130296652+STASKO24@users.noreply.github.com>
2026-07-12 22:51:49 +0300

    Add gsap animation to preloader

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ gsap.registerPlugin(SplitText, CustomEase, Flip);
 
 document.addEventListener("DOMContentLoaded", () => {
     const introImages = document.querySelectorAll('.intro-img');
+    const preloaderOverlay = document.querySelector('.preloader-overlay');
+    const preloader = preloaderOverlay.querySelector('.preloader');
 
 
     const introImgWidth = introImages[0].offsetWidth;
@@ -30,6 +32,27 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     const tl = gsap.timeline({delay: 1});
+
+    tl.from(preloader, {
+        scaleX: 0,
+        duration: 1.2,
+        ease: "power2.out",
+        onComplete: () => {
+            gsap.set(preloader, {transformOrigin: "right"})
+        }
+    })
+
+    tl.to(preloader, {
+        scaleX: 0,
+        ease: "power2.out",
+        duration: 1,
+    })
+    tl.to(preloaderOverlay, {
+        clipPath: 'polygon(0% 0%, 100% 0, 100% 0%, 0% 0%)',
+        duration: 1.5,
+        ease: "power2.out",
+    }, '<0.75')
+
     introImages.forEach((img) => {
         tl.to(img, {
             x: parseFloat(img.dataset.centredX),
